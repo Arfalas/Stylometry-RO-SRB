52d4968caaa831635ff90c986f47e6b60c635e49
Marko Milosev <101294904+Arfalas@users.noreply.github.com>
2023-03-13 22:17:02 +0100

    Update Stylometry_SRB.py

--- Stylometry_SRB.py
+++ Stylometry_SRB.py
@@ -1,34 +1,42 @@
 
-#Articles and texts are presented as dictionaries (Key:Author ; Value:.txt number)
-articles = { 'Ljotic' : [1, 2, 3, 4, 5],
-            'Unknown' : [6, 7],
-             'Nedic' : [8, 9] }
+from encodings import utf_8
 
-# Opens .txt files, converts them to strings and groups them by author
-def txt_files_to_strings(filenames):
-    strings = []
-    for filename in filenames:
-        with open(f'C:/Users/No Name/OneDrive/Desktop/Stylometry Project/Lotic_{filename}.txt', 'r', encoding="utf_8") as f:
-            strings.append(f.read())
-    return '\n'.join(strings)
+txtfile1 = open("C:/Users/No Name/OneDrive/Desktop/Stylometry Project/Ljotic.txt", "r", encoding="utf_8")
+Ljotic = txtfile1.read()
+txtfile2 = open("C:/Users/No Name/OneDrive/Desktop/Stylometry Project/Nedic.txt", "r", encoding="utf_8")
+Nedic = txtfile2.read()
+txtfile3 = open("C:/Users/No Name/OneDrive/Desktop/Stylometry Project/Unknown.txt", "r", encoding="utf_8")
+Unknown = txtfile3.read()
 
-strings_by_creator = {}
-for creator, files in articles.items():
-    strings_by_creator[creator] = txt_files_to_strings(files)
+import nltk 
 
-# Testing if the strings are properly loaded
-for creator in articles:
-    print(strings_by_creator[creator][:100])
+lower_case_Ljotic = Ljotic.lower()
+lower_case_Disputed = Unknown.lower()
 
-# Tokenization and POS for the Serbian cyrilic text
-txt_file = open(f'C:/Users/No Name/OneDrive/Desktop/Stylometry Project/Lotic_1.txt', 'r', encoding="utf_8")
-source_txt = txt_file.read()
+lower_case_tokenized_Ljotic = nltk.word_tokenize(lower_case_Ljotic)
+lower_case_tokenized_Disputed = nltk.word_tokenize(lower_case_Disputed)
 
-import classla 
-classla.download('sr')
-nlp = classla.Pipeline('sr', processors = 'tokenize, pos')
-doc = nlp(source_txt)
+joint_corpus = (lower_case_tokenized_Ljotic + lower_case_tokenized_Disputed)
 
-print(doc.to_conll())
+frequent_words_list = nltk.FreqDist(joint_corpus)
+most_common_words_joint = list(frequent_words_list.most_common(500))
 
+#print(most_common_words_joint)
 
+proportion_by_author = (len(lower_case_tokenized_Ljotic) / len(joint_corpus))
+
+chisquared = 0
+
+for word,joint_count in most_common_words_joint:
+
+    author_count = lower_case_tokenized_Ljotic.count(word)
+    unknown_count = lower_case_tokenized_Disputed.count(word)
+
+expected_author_count = joint_count * proportion_by_author
+expected_unknown_count = joint_count * (1-proportion_by_author)
+
+chisquared += ((author_count - expected_author_count) * (author_count - expected_author_count) / expected_author_count)
+
+chisquared += ((unknown_count - expected_unknown_count) * (unknown_count - expected_unknown_count) / expected_unknown_count)
+
+print('The expected Chi-squared statistic for candidate Ljotic is', chisquared)
